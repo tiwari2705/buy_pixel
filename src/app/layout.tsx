30a7faee24fbe45f0b4c393f81dda71e52713395
Harsh Tiwari <harshtiwari230202@gmail.com>
2026-08-12 21:24:02 +0530

    Integrate Google Analytics into layout
    Added Google Analytics integration with tracking script.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 					</main>
 					<SiteFooter />
 				</div>
+
 				<Analytics />
+
+				{/* Google Analytics */}
+				<Script
+					src="https://www.googletagmanager.com/gtag/js?id=G-9ZZ0S726KB"
+					strategy="afterInteractive"
+				/>
+
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){window.dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-9ZZ0S726KB');
+					`}
+				</Script>
 			</body>
 		</html>
 	)
